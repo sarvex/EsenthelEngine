f73a400a926916371f39c5e6c81496aedcef6368
GregSlazinski <esenthel@hotmail.com>
2022-05-11 18:02:55 +0700

    ok

--- ThirdPartyLibs/BC/ispc_texcomp/ispc_texcomp.h
+++ ThirdPartyLibs/BC/ispc_texcomp/ispc_texcomp.h
@@ -1,39 +1,28 @@
-/*
-  Copyright (c) 2013-2015, Intel Corporation
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are
-  met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-    * Neither the name of Intel Corporation nor the names of its
-      contributors may be used to endorse or promote products derived from
-      this software without specific prior written permission.
-
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
-*/
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016-2019, Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
 
 #include <stdint.h>
 
-struct rgba_surface 
+struct rgba_surface
 {
     uint8_t* ptr;
     int32_t width;
@@ -114,18 +103,22 @@ extern "C" void GetProfile_astc_alpha_slow(astc_enc_settings* settings, int bloc
 extern "C" void ReplicateBorders(rgba_surface* dst_slice, const rgba_surface* src_tex, int x, int y, int bpp);
 
 /*
-	Notes:
-	  - input width and height need to be a multiple of block size
-      - LDR input is 32 bit/pixel (sRGB), HDR is 64 bit/pixel (half float)
-	  - dst buffer must be allocated with enough space for the compressed texture:
-		4 bytes/block for BC1/ETC1, 8 bytes/block for BC3/BC6H/BC7/ASTC
-		the blocks are stored in raster scan order (natural CPU texture layout)
-	  - you can use GetProfile_* functions to select various speed/quality tradeoffs.
-	  - the RGB profiles are slightly faster as they ignore the alpha channel
+Notes:
+    - input width and height need to be a multiple of block size
+    - LDR input is 32 bit/pixel (sRGB), HDR is 64 bit/pixel (half float)
+        - for BC4 input is 8bit/pixel (R8), for BC5 input is 16bit/pixel (RG8)
+    - dst buffer must be allocated with enough space for the compressed texture:
+        - 8 bytes/block for BC1/BC4/ETC1,
+        - 16 bytes/block for BC3/BC5/BC6H/BC7/ASTC
+    - the blocks are stored in raster scan order (natural CPU texture layout)
+    - use the GetProfile_* functions to select various speed/quality tradeoffs
+    - the RGB profiles are slightly faster as they ignore the alpha channel
 */
 
 extern "C" void CompressBlocksBC1(const rgba_surface* src, uint8_t* dst);
 extern "C" void CompressBlocksBC3(const rgba_surface* src, uint8_t* dst);
+extern "C" void CompressBlocksBC4(const rgba_surface* src, uint8_t* dst);
+extern "C" void CompressBlocksBC5(const rgba_surface* src, uint8_t* dst);
 extern "C" void CompressBlocksBC6H(const rgba_surface* src, uint8_t* dst, bc6h_enc_settings* settings);
 extern "C" void CompressBlocksBC7(const rgba_surface* src, uint8_t* dst, bc7_enc_settings* settings);
 extern "C" void CompressBlocksETC1(const rgba_surface* src, uint8_t* dst, etc_enc_settings* settings);
