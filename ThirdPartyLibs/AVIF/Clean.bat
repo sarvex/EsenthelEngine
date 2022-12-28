e7e0ec7dd4c8fe5e68f290508072331ea0929f0c
GregSlazinski <esenthel@hotmail.com>
2022-12-28 23:08:46 +0700

    ok

--- ThirdPartyLibs/AVIF/Clean.bat
+++ ThirdPartyLibs/AVIF/Clean.bat
@@ -10,3 +10,13 @@ rmdir /s /q Android\avif.dir
 rmdir /s /q Android\CMakeFiles
 rmdir /s /q Android\contrib
 rmdir /s /q Android\x64
+
+rmdir /s /q Mac\build
+rmdir /s /q Mac\CMakeFiles
+rmdir /s /q Mac\CMakeScripts
+rmdir /s /q Mac\contrib
+rmdir /s /q Mac\Debug
+del Mac\cmake_install.cmake
+del Mac\CMakeCache.txt
+del Mac\libavif.pc
+del Mac\libavif-config-version.cmake
