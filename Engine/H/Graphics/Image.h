5c42b8c8ce07c40609030805a746ce33a710d252
GregSlazinski <esenthel@hotmail.com>
2023-01-02 00:42:14 +0700

    ok

--- Engine/H/Graphics/Image.h
+++ Engine/H/Graphics/Image.h
@@ -833,6 +833,7 @@ Vec      CubeFaceToDir(Flt x, Flt y, Int res, DIR_ENUM cube_face); // convert im
 
 #if EE_PRIVATE
 #define MAX_MIP_MAPS 32
+INLINE Bool CheckMipNum(Int mips) {return InRange(mips, MAX_MIP_MAPS+1);} // +1 because this checks number of elements
 struct ImageThreadsClass : Threads
 {
    ImageThreadsClass& init()
