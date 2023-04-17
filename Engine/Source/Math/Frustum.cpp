8c425e982b81c3e2b25599fec94515331f8dc918
GregSlazinski <esenthel@hotmail.com>
2023-04-17 11:54:45 +0700

    ok

--- Engine/Source/Math/Frustum.cpp
+++ Engine/Source/Math/Frustum.cpp
@@ -914,7 +914,8 @@ void FrustumClass::getIntersectingSphereAreas(MemPtr<SphereArea> area_pos, C Sph
             if(oriented_point_ok[edge.x]!=oriented_point_ok[edge.y]) // if one point is OK and other NOT (one above min height and one under)
             {
              C auto &a=oriented_point[edge.x], &b=oriented_point[edge.y];
-               // FIXME
+               Dbl hit=1, delta=b.z-a.z, frac=(hit-a.z)/delta; // calculate Lerp frac to intersect at value Z=1
+               projected_point[projected_points++]=Lerp(a.xy, b.xy, frac); // add point XY at Z=1 to projected points
             }
          }
          CreateConvex2D(convex_points, projected_point, projected_points); if(!convex_points.elms())goto next;
