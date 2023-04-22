dee00a23b4acb9d8edc8743a01c60572f0442ab8
GregSlazinski <esenthel@hotmail.com>
2023-04-23 00:33:48 +0700

    ok

--- Engine/Source/Graphics/Image Pixel.cpp
+++ Engine/Source/Graphics/Image Pixel.cpp
@@ -4079,58 +4079,40 @@ Vec4 Image::cubeColorFCubicFast(C Vec &dir)C
       auto pitch    =softPitch   (0);
       auto byte_pp  =bytePP      ( );
 
-      Vec2  xy; DIR_ENUM face=DirToCubeFacePixel(dir, w(), xy); // calculate main face
-      auto  face_data=data + face*face_size;
+      SphereArea sa;
+      Vec2  xy; sa.side=DirToCubeFacePixel(dir, w(), xy); // calculate main face
+      auto  face_data=data + sa.side*face_size;
       VecI2 xyi=Floor(xy); xy-=xyi; xyi--;
-
-      Vec4 c[4][4];
+      Vec   rgb   =0;
+      Vec4  color =0;
+      Flt   weight=0;
+      Flt   weights_x[4]={Sqr(xy.x+1), Sqr(xy.x), Sqr(xy.x-1), Sqr(xy.x-2)};
+      Flt   weights_y[4]={Sqr(xy.y+1), Sqr(xy.y), Sqr(xy.y-1), Sqr(xy.y-2)};
       FREPD(sy, 4)
       {
-         Int y=xyi.y+sy;
+         sa.y=xyi.y+sy;
+         Flt weight_y=weights_y[sy];
          FREPD(sx, 4)
          {
-            Int x=xyi.x+sx;
-            CPtr d;
-            if(InRange(x, w())
-            && InRange(y, h()))
-               d=face_data + y*pitch + x*byte_pp;else // if coords in range then use main face
-            { // coords out of range
-               Vec  dir=CubeFacePixelToDir(x, y, w(), face); // convert to direction vector
-               Vec2 xy; DIR_ENUM face=DirToCubeFacePixel(dir, w(), xy); // convert direction vector to secondary face
-               Int  x=Mid(Round(xy.x), 0, w()-1);
-               Int  y=Mid(Round(xy.y), 0, h()-1);
-
-               d=data + face*face_size + y*pitch + x*byte_pp;
+            sa.x=xyi.x+sx;
+            Flt weight_x=weights_x[sx];
+            Flt wgt=weight_x+weight_y; if(wgt<Sqr(CUBIC_FAST_RANGE))
+            {
+               CPtr d;
+               if(InRange(sa.x, w())
+               && InRange(sa.y, h()))
+                  d=face_data + sa.y*pitch + sa.x*byte_pp;else // if both coords in range then use main face
+               if(InRange(sa.x, w())
+               || InRange(sa.y, h()))
+               { // if at least one coord in range then wrap
+                  SphereArea sa1; Wrap(sa1, sa, w());
+                  d=data + sa1.side*face_size + sa1.y*pitch + sa1.x*byte_pp;
+               }else
+                  continue; // if both coords out of range then skip
+               wgt=CubicFast2(wgt); Add(color, rgb, ImageColorF(d, hwType()), wgt, alpha_weight); weight+=wgt;
             }
-            c[sy][sx]=ImageColorF(d, hwType());
          }
       }
-
-      Vec  rgb   =0;
-      Vec4 color =0;
-      Flt  weight=0, w,
-           x=xy.x, y=xy.y,
-           x0w=Sqr(x+1), x1w=Sqr(x), x2w=Sqr(x-1), x3w=Sqr(x-2),
-           y0w=Sqr(y+1), y1w=Sqr(y), y2w=Sqr(y-1), y3w=Sqr(y-2);
-      w=x0w+y0w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[0][0], w, alpha_weight); weight+=w;}
-      w=x1w+y0w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[0][1], w, alpha_weight); weight+=w;}
-      w=x2w+y0w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[0][2], w, alpha_weight); weight+=w;}
-      w=x3w+y0w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[0][3], w, alpha_weight); weight+=w;}
-
-      w=x0w+y1w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[1][0], w, alpha_weight); weight+=w;}
-      w=x1w+y1w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[1][1], w, alpha_weight); weight+=w;}
-      w=x2w+y1w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[1][2], w, alpha_weight); weight+=w;}
-      w=x3w+y1w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[1][3], w, alpha_weight); weight+=w;}
-
-      w=x0w+y2w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[2][0], w, alpha_weight); weight+=w;}
-      w=x1w+y2w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[2][1], w, alpha_weight); weight+=w;}
-      w=x2w+y2w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[2][2], w, alpha_weight); weight+=w;}
-      w=x3w+y2w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[2][3], w, alpha_weight); weight+=w;}
-
-      w=x0w+y3w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[3][0], w, alpha_weight); weight+=w;}
-      w=x1w+y3w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[3][1], w, alpha_weight); weight+=w;}
-      w=x2w+y3w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[3][2], w, alpha_weight); weight+=w;}
-      w=x3w+y3w; if(w<Sqr(CUBIC_FAST_RANGE)){w=CubicFast2(w); Add(color, rgb, c[3][3], w, alpha_weight); weight+=w;}
       Normalize(color, rgb, weight, alpha_weight, ALPHA_LIMIT_CUBIC_FAST);
       return color;
    }
